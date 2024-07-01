5f79adf764e81049bcbe23df396bc66316639859
Shrestha Mukherjee <134909641+Shrestha005@users.noreply.github.com>
2024-07-01 17:34:26 +0000

    commit

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,12 @@
 import One from "./1"
 
 function App() {
+  const username = "shres005"
 
   return (
     <>
     <One/>
+    <h1>user is {username}</h1>
     <h1>reacttttt is here</h1>
     <p>test p tag</p>
     </>
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// import {jsx as _jsx} from "react/jsx-runtime.js"
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App:)</h1>
+    </div>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
