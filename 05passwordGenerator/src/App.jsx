f7a891a00f846ddf8fe3b71153a02cd7ab3489ed
Shrestha Mukherjee <shresthamukh07@gmail.com>
2024-07-03 22:14:11 +0530

    commit

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,31 +1,44 @@
-import { useState ,useCallback } from 'react'
-import './App.css'
+import { useState, useCallback, useEffect, useRef } from 'react'
+
+
 
 function App() {
-  const [length, setLength] = useState(8) 
-  const [numberAllowed, setnumberAllowed] = useState(false) 
-  const [charAllowed, setcharAllowed] = useState(false)
-  const [password, setPassword] = useState("") 
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false)
+  const [password, setPassword] = useState("")
 
-  const passwordGenerator = useCallback(() =>  {
+  //useRef hook
+  const passwordRef = useRef(null)
+
+  const passwordGenerator = useCallback(() => {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-
     if (numberAllowed) str += "0123456789"
-    if (charAllowed) str += "!@#$%&*."
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
 
-    for (let i = 1; i <= array.length; i++) {
+    for (let i = 1; i <= length; i++) {
       let char = Math.floor(Math.random() * str.length + 1)
-      pass = str.charAt(char)
+      pass += str.charAt(char)
+      
     }
 
     setPassword(pass)
 
 
-  }, [length , numberAllowed , charAllowed , setPassword])
+  }, [length, numberAllowed, charAllowed, setPassword])
 
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  useEffect(() => {
+    passwordGenerator()
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
-    <>
+    
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
       <h1 className='text-white text-center my-3'>Password generator</h1>
     <div className="flex shadow rounded-lg overflow-hidden mb-4">
@@ -79,7 +92,7 @@ function App() {
       </div>
     </div>
 </div>
-    </>
+    
   )
 }
 
